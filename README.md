973348caeed9e6ac6a81a1ea3ac4bda824294731
VadimKhurs <vadimhursevich@gmail.com>
2025-02-06 12:51:49 +0300

    gcp

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # PotatoDiseaseClassification
+
+Command to start tf-serving
+
+docker run -t --rm -p 8501:8501 \
+  -v /home/vadim/Desktop/prog/ML/Projects/PotatoDiseaseClassification/models:/models \
+  -e MODEL_NAME=potato_disease_classifier1 \
+  tensorflow/serving:latest \
+  --rest_api_port=8501
+
+
+gcloud functions deploy predict --runtime python38 --trigger-http --memory 512 --project potato-disiase-classification
--- api/main.py
+++ api/main.py
@@ -26,9 +26,15 @@ async def predict(
     image = read_file_as_image(await file.read())
     img_batch = np.expand_dims(image, 0)
 
-    prediction = MODEL.predict(img_batch)
+    predictions = MODEL.predict(img_batch)
 
-    return prediction
+    predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
+    confidence = np.max(predictions[0])
+
+    return {
+        'class': predicted_class,
+        'confidence': float(confidence)
+    }
 
 
 if __name__ == "__main__":
